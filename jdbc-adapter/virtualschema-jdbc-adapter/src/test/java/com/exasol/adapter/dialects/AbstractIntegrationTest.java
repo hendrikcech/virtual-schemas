afe5281853e87b3c05bb2fb54aafb4f7b6f73a8e
Hendrik Cech <hendrik.cech@gmail.com>
2018-05-14 14:09:55 +0200

    Add JAVA_TOOL_OPTIONS urandom to jdbc_adapter

--- jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/AbstractIntegrationTest.java
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/AbstractIntegrationTest.java
@@ -67,8 +67,9 @@ public class AbstractIntegrationTest {
     public static void createJDBCAdapter(Connection conn, List<String> jarIncludes) throws SQLException {
         Statement stmt = conn.createStatement();
         stmt.execute("CREATE SCHEMA IF NOT EXISTS ADAPTER");
-        String sql = "CREATE OR REPLACE JAVA ADAPTER SCRIPT ADAPTER.JDBC_ADAPTER AS\n";
-        sql += "  %scriptclass com.exasol.adapter.jdbc.JdbcAdapter;\n";
+        String sql = "CREATE OR REPLACE JAVA ADAPTER SCRIPT ADAPTER.JDBC_ADAPTER AS\n" +
+                "  %env JAVA_TOOL_OPTIONS=\"-Djava.security.egd=file:/dev/./urandom\";\n" +
+                "  %scriptclass com.exasol.adapter.jdbc.JdbcAdapter;\n";
         for (String includePath : jarIncludes) {
             sql += "  %jar " + includePath + ";\n";
         }
