9599856da3cafd61f419fc05f4ec6dbfc87ead10
Hendrik Cech <hendrik.cech@gmail.com>
2018-06-04 13:09:34 +0200

    Move content of travis.yml to jdbc-adapter/run_integration_tests.sh

---
+++ jdbc-adapter/run_integration_tests.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script runs the integration tests for an Exasol virtual schema. Docker is
+# used to execute the Exasol container.
+
+location="$(dirname "$0")"
+config="$location/integration-test-data/integration-test-travis.yaml"
+
+cd $location
+
+docker pull exasol/docker-db:latest
+docker run --name exasoldb \
+    -p 8888:8888 \
+    -p 6583:6583 \
+    --detach \
+    --privileged \
+    --stop-timeout 120 \
+    exasol/docker-db:latest
+
+# Wait for EXAConf to be generated
+while [ -z "$W_PW" ]; do
+    W_PW=$(docker exec exasoldb cat /exa/etc/EXAConf | awk '/WritePasswd/{ print $3; }' | base64 -d);
+    sleep 1;
+done
+sed -i -e "s/BUCKET_FS_PASSWORD/$W_PW/" "$config"
+
+mvn clean package
+
+# Wait for exaudfclient to be extracted and available
+while [ $(docker exec exasoldb find /exa/data/bucketfs/bfsdefault/.dest -name exaudfclient -printf '.' | wc -c) -lt 1 ]; do
+    sleep 1;
+done
+
+# Upload virtualschema-jdbc-adapter jar and wait a bit to make sure it's available
+mvn pre-integration-test -DskipTests -Pit -Dintegrationtest.configfile="$config"
+sleep 60
+
+mvn verify -Pit -Dintegrationtest.configfile="$config"
